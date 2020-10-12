f1bb3915b1381db7244d17919d27f914dd02ed63
William MR <william.munozr@gmail.com>
2020-10-11 21:24:21 -0500

    Deployment Test

--- deployments/docker-entrypoint.sh
+++ deployments/docker-entrypoint.sh
@@ -1,21 +1,80 @@
-#!/usr/bin/env bash
+#!/bin/sh
 
-set -euo pipefail
+# Use gosu if the container started with root privileges
+UID="$(id -u)"
+[ "$UID" -eq 0 ] && GOSU="gosu codimd" || GOSU=""
 
-if [[ "$#" -gt 0 ]]; then
-    exec "$@"
-    exit $?
+if [ "$HMD_DB_URL" != "" ] && [ "$CMD_DB_URL" = "" ]; then
+    CMD_DB_URL="$HMD_DB_URL"
 fi
 
-# check database and redis is ready
-pcheck -env CMD_DB_URL
-
-# run DB migrate
-NEED_MIGRATE=${CMD_AUTO_MIGRATE:=true}
-
-if [[ "$NEED_MIGRATE" = "true" ]] && [[ -f .sequelizerc ]] ; then
-    npx sequelize db:migrate
+if [ "$HMD_IMAGE_UPLOAD_TYPE" != "" ] && [ "$CMD_IMAGE_UPLOAD_TYPE" = "" ]; then
+    CMD_IMAGE_UPLOAD_TYPE="$HMD_IMAGE_UPLOAD_TYPE"
 fi
 
-# start application
-node app.js
+DOCKER_SECRET_DB_URL_FILE_PATH="/run/secrets/dbURL"
+
+if [ -f "$DOCKER_SECRET_DB_URL_FILE_PATH" ]; then
+    CMD_DB_URL="$(cat $DOCKER_SECRET_DB_URL_FILE_PATH)"
+fi
+
+if [ "$CMD_DB_URL" = "" ]; then
+    CMD_DB_URL="postgres://hackmd:hackmdpass@hackmdPostgres:5432/hackmd"
+fi
+
+export CMD_DB_URL
+
+DB_SOCKET=$(echo ${CMD_DB_URL} | sed -e 's/.*:\/\//\/\//' -e 's/.*\/\/[^@]*@//' -e 's/\/.*$//')
+
+if [ "$DB_SOCKET" != "" ]; then
+    dockerize -wait "tcp://${DB_SOCKET}" -timeout 30s
+fi
+
+$GOSU ./node_modules/.bin/sequelize db:migrate
+
+# Print warning if local data storage is used but no volume is mounted
+[ "$CMD_IMAGE_UPLOAD_TYPE" = "filesystem" ] && { mountpoint -q ./public/uploads || {
+    echo "
+        #################################################################
+        ###                                                           ###
+        ###                        !!!WARNING!!!                      ###
+        ###                                                           ###
+        ###        Using local uploads without persistence is         ###
+        ###            dangerous. You'll loose your data on           ###
+        ###              container removal. Check out:                ###
+        ###  https://docs.docker.com/engine/tutorials/dockervolumes/  ###
+        ###                                                           ###
+        ###                       !!!WARNING!!!                       ###
+        ###                                                           ###
+        #################################################################
+    ";
+} ; }
+
+# Change owner and permission if filesystem backend is used and user has root permissions
+if [ "$UID" -eq 0 ] && [ "$CMD_IMAGE_UPLOAD_TYPE" = "filesystem" ]; then
+    if [ "$UID" -eq 0 ]; then
+        chown -R codimd ./public/uploads
+        chmod 700 ./public/uploads
+    else
+        echo "
+            #################################################################
+            ###                                                           ###
+            ###                        !!!WARNING!!!                      ###
+            ###                                                           ###
+            ###        Container was started without root permissions     ###
+            ###           and filesystem storage is being used.           ###
+            ###        In case of filesystem errors these need to be      ###
+            ###                      changed manually                     ###
+            ###                                                           ###
+            ###                       !!!WARNING!!!                       ###
+            ###                                                           ###
+            #################################################################
+        ";
+    fi
+fi
+
+# Sleep to make sure everything is fine...
+sleep 3
+
+# run
+exec $GOSU "$@"
